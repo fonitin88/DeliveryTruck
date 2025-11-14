4e2b13912a71ec290dfa52d2b5bed9b948aae801
fonitin88 <92967999+fonitin88@users.noreply.github.com>
2025-11-14 14:23:59 +0800

    add color select in Menu

--- Assets/Van Collection/Scripts/Controller/ColorController.cs
+++ Assets/Van Collection/Scripts/Controller/ColorController.cs
@@ -8,25 +8,31 @@ public class ColorController : MonoBehaviour
     //把data view gameobj button 帶進去 
     public ColorData colorData;
     public ColorView colorView;
-    public GameObject targetObject;
+    public Button resetBtn;
     public Button redBtn;
     public Button yellowBtn;
     public Button blueBtn;
 
     void Start()
     {
-        //view中的物件
 
         // 從資料庫載入顏色和 更新顏色
+        colorData.seletedColor = PlayerPrebManager.LoadColor();
+        colorView.UpdateColor(colorData.seletedColor);
 
         //按鈕寫上功能
-
+        redBtn.onClick.AddListener(() => OnColorSelected(Color.red));
+        yellowBtn.onClick.AddListener(() => OnColorSelected(Color.yellow));
+        blueBtn.onClick.AddListener(() => OnColorSelected(Color.blue));
+        resetBtn.onClick.AddListener(() => OnColorSelected(Color.white));
 
     }
     //選了顏色後更新到data / view / save
-    void OnColorSelected()
+    void OnColorSelected(Color color)
     {
-
+        colorData.seletedColor = color;
+        colorView.UpdateColor(color);
+        PlayerPrebManager.SaveColor(color);
     }
 
 }
