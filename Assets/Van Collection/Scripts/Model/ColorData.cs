e4e2b9dbee27eecbe59c45d7e323f71c10777da7
fonitin88 <92967999+fonitin88@users.noreply.github.com>
2025-11-14 09:20:47 +0800

    Revert "Initial commit"
    This reverts commit 700a5512c915de17cce0b5890fa04b8371d6bc54.

--- Assets/Van Collection/Scripts/Model/ColorData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ColorData", menuName = "Scriptable Objects/ColorData")]
-public class ColorData : ScriptableObject
-{
-    public Color seletedColor = Color.red;
-}
