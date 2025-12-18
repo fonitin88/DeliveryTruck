65176edbbf913bb0bc62eed99511d12cd8e8e2d0
fonitin88 <92967999+fonitin88@users.noreply.github.com>
2025-12-18 13:00:04 +0800

    移動script

--- Assets/Scripts/Core/IDamageable.cs
+++ Assets/Scripts/Core/IDamageable.cs
@@ -1,4 +1,5 @@
 
+//一個標籤的概念 掛上這個標籤就代表可takedamage
 public interface IDamageable
 {
     void TakeDamage(int amount);
--- Assets/Scripts/Core/VFX_Damage.cs
+++ Assets/Scripts/Core/VFX_Damage.cs
@@ -1,22 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class VFX_Damage : MonoBehaviour
 {
     [SerializeField] int damage = 1;
+    float T;
+    float plusDamage;
 
     public void SetDamage(int value)
     {
         damage = value;
     }
-
+    //改成只要在這個collision 內 都會一直扣，離開才不會扣，每秒一直扣
+    //已經在trigger內
+    //每秒扣
     void OnTriggerEnter(Collider other)
     {
-        //有沒有實作 IDamageable 的 component
+        //有沒有IDamageable 的 component
         IDamageable target = other.GetComponent<IDamageable>();
         if (target != null)
         {
+
             target.TakeDamage(damage);
 
+
         }
+    }
+    IEnumerator damageDuring()
+    {
+
+        yield return new WaitForSeconds(T);
+        plusDamage += damage;
+
     }
 }
